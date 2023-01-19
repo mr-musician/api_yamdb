f601d8adbf94f35ed96f43b97297062ed72730a2
Борис Жуков <boris10264@gmail.com>
2023-01-19 23:12:18 +0300

    add_some_models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,21 +4,52 @@ from django.db import models
 User = get_user_model()
 
 
-class Title(models.Model):
-    pass
-
-
-class Review(models.Model):
-    pass
-
-
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.DO_NOTHING  # ?
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    # title_id =
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    score = models.IntegerField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
 
 class Comment(models.Model):
-    pass
+    # review_id =
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
