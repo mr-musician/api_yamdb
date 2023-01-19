246e60021e0e586a231cb2650ae880d72d2a9dc6
gloth <somov.feodor.mail@gmail.com>
2023-01-20 02:22:26 +0300

    search fields added + other fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
 from reviews.models import Category, Titles, Genre
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .serializers import CategorySerializer, TitlesSerializer, GenreSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
+    permission_classes = () # Админ или только чтение
+    filter_backends = (filters.SearchFilter)
+    search_fields = ['name']
+    lookup_field = ['slug']
+    
     def perform_create(self, serializer):
         return super().perform_create(serializer)
 
 class TitlesViewSet(viewsets.ModelViewSet):
     queryset = Titles.objects.all()
     serializer_class = TitlesSerializer
+    permission_classes = [] # Админ или только чтение
+    filter_backends = (filters.SearchFilter)
+    search_fields = ['name', 'year', 'genre', 'category']
+    lookup_field = ['slug']
 
     def perform_create(self, serializer):
         return super().perform_create(serializer)
@@ -20,6 +27,10 @@ class TitlesViewSet(viewsets.ModelViewSet):
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = [] # Админ или только чтение
+    filter_backends = (filters.SearchFilter)
+    search_fields = ['name']
+    lookup_field = ['slug']
 
     def perform_create(self, serializer):
         return super().perform_create(serializer)
