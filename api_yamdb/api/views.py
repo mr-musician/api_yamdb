0810f71e1905c9445db84e14fd29f89e21e8bf3b
SabjBrus <104594318+SabjBrus@users.noreply.github.com>
2023-01-22 19:00:17 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 
 from api.serializers import CommentSerializer, ReviewSerializer
